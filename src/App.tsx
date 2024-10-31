291ab63fa544d8cf5a17c90522168496747ee916
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-31 09:04:43 -0400

    Phase 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-
+import './index.css'
 function App() {
   
 
   return (
     <>
-      <h1>Crypto App</h1>
+      <div className="container">
+        <h1 className='app-title'>
+          <span>Cryptocurrency</span> coster
+        </h1>
+
+        <div className="content">
+
+        </div>
+      </div>
     </>
   )
 }
