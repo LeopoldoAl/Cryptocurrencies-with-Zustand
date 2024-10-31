c33533ddc672ad708ff2e3ded3e08b016aedcd1c
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-31 12:20:23 -0400

    Phase 26

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,28 @@
 import { create } from "zustand";
 import axios from "axios";
 import { CryptoCurrrenciesResponseSchema } from "./schemas/crytp-schema";
+import { Cryptocurrency } from "./types";
+
+type CryptoStore = {
+    crytocurrencies: Cryptocurrency[]
+    fetchCrypto: () => Promise<void>
+}
 
 async function getCryptos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
     const {data: { Data }} = await axios(url)
     const result = CryptoCurrrenciesResponseSchema.safeParse(Data)
-    console.log(result)
+    
+    if (result.success) {
+        return result.data
+    }
 }
-export const useCryptoStore = create(() => ({
-    fetchCrypto: () => {
-        getCryptos()
+export const useCryptoStore = create<CryptoStore>((set) => ({
+    crytocurrencies: [],
+    fetchCrypto: async () => {
+        const crytoCurrencies = await getCryptos()
+        set(() => ({
+            crytocurrencies: crytoCurrencies
+        }))
     }
 }))
