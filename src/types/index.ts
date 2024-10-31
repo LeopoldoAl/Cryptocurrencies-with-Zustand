44f141ed3941dbc5e34c27dbcfc72f1bbe647f2d
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-31 12:20:04 -0400

    Phase 25

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
-import { CurrencySchema } from '../schemas/crytp-schema'
+import { CurrencySchema, CryptoCurrrencyResponseSchema } from '../schemas/crytp-schema'
 
 export type Currency = z.infer<typeof CurrencySchema>
+export type Cryptocurrency = z.infer<typeof CryptoCurrrencyResponseSchema>
