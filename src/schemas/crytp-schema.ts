6f5cb27123ca7d9f355e73b38a8f396ba9b7c85d
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-31 11:49:21 -0400

    Phase 24

--- src/schemas/crytp-schema.ts
+++ src/schemas/crytp-schema.ts
@@ -4,3 +4,12 @@ export const CurrencySchema = z.object({
     code: z.string(),
     name: z.string()
 })
+
+export const CryptoCurrrencyResponseSchema = z.object({
+    CoinInfo: z.object({
+        FullName: z.string(),
+        Name: z.string()
+    })
+})
+
+export const CryptoCurrrenciesResponseSchema = z.array(CryptoCurrrencyResponseSchema)
