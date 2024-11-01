becbfe5ced94026eb5e58b91ce201d7c170c0641
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-01 13:01:13 -0400

    Phase 49

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -7,14 +7,21 @@ export default function CryptoPriceDisplay() {
     const hasResult = useMemo(() => !Object.values(result).includes(''),[result.PRICE])
     
   return (
-    <div>
+    <div className="result-wrapper">
         {hasResult && (
             <>
                 <h2>Valuation</h2>
                 <div className="result">
-
+                    <img 
+                        src={`https://cryptocompare.com/${result.IMAGEURL}`} 
+                        alt="Cryptocurrency Image" 
+                    />
                     <div>
                         <p>The price is about: <span>{result.PRICE}</span></p>
+                        <p>The highest price in the day: <span>{result.HIGHDAY}</span></p>
+                        <p>The lowest price in the day: <span>{result.LOWDAY}</span></p>
+                        <p>Last 24 hours change: <span>{result.CHANGEPCT24HOUR}</span></p>
+                        <p>The last update: <span>{result.LASTUPDATE}</span></p>
                     </div>
                 </div>
             </>
